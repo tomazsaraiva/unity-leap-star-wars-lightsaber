5d31713adbe3538ad04205e476fff91fc3113b98
Tomaz Saraiva <tomaz.saraiva@gmail.com>
2017-05-10 23:21:34 +0100

    initial commit with project and gitignore

---
+++ Assets/Scripts/LeapController.cs
@@ -0,0 +1,130 @@
+﻿using Leap;
+using Leap.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LeapController : MonoBehaviour
+{
+    private float MIN_DISABLE_TIME = 2.0f;
+
+    [SerializeField]
+    private GameObject _root;
+
+    [SerializeField]
+    private Lightsaber _lightsaber;
+
+    [SerializeField]
+    private GameObject _grabPoint;
+
+    [SerializeField]
+    private float _grabDuration;
+
+
+    private LeapProvider _leapProvider;
+
+
+    private bool _grab;
+    private float _deltaPosition;
+    private float _deltaRotation;
+
+    private bool _hasLightSaber;
+    private bool _isEnabled;
+
+    private bool _disable;
+    private float _disableTime;
+
+
+    #region MONOBEHAVIOUR
+    
+    void Start()
+    {
+        _leapProvider = FindObjectOfType<LeapProvider>();
+    }
+
+    private void Update()
+    {
+        if(_grab)
+        {
+            _root.transform.localPosition = Vector3.MoveTowards(_root.transform.localPosition, Vector3.zero, _deltaPosition * Time.deltaTime);
+            _root.transform.localRotation = Quaternion.RotateTowards(_root.transform.localRotation, Quaternion.identity, _deltaRotation * Time.deltaTime);
+
+            if(_root.transform.localPosition == Vector3.zero)
+            {
+                _hasLightSaber = true;
+                _grab = false;
+            }
+        }
+
+        if(_hasLightSaber && _isEnabled)
+        {
+            if(_leapProvider.CurrentFrame != null && 
+                _leapProvider.CurrentFrame.Hands != null &&
+                _leapProvider.CurrentFrame.Hands.Count > 0 &&
+                _leapProvider.CurrentFrame.Hands[0].PalmVelocity.Magnitude > 0.1f)
+            {
+                _lightsaber.Swing();
+            }
+        }
+
+        if(_disable && Time.time - _disableTime > MIN_DISABLE_TIME)
+        {
+            _disable = false;
+
+            _lightsaber.Close();
+        }
+    }
+
+    #endregion
+
+
+    public void Grab()
+    {
+        if(!_hasLightSaber)
+        {
+            _root.transform.SetParent(_grabPoint.transform);
+
+            _deltaPosition = Vector3.Distance(_root.transform.localPosition, Vector3.zero) / _grabDuration;
+            _deltaRotation = Quaternion.Angle(_root.transform.localRotation, Quaternion.identity) / _grabDuration;
+
+            _lightsaber.Grab();
+
+            _grab = true;
+        }
+    }
+    
+
+    public void Enable(bool enabled)
+    {
+        if(_hasLightSaber && enabled != _isEnabled)
+        {
+            if(enabled)
+            {
+                _lightsaber.Open();
+            }
+            else
+            {
+                _disableTime = Time.time;
+                _disable = true;
+            }
+
+            _isEnabled = enabled;
+        }
+
+        if(_disable && enabled)
+        {
+            _disable = false;
+        }
+    }
+
+    public void Release()
+    {
+        if(_hasLightSaber)
+        {
+            _hasLightSaber = false;
+
+            _lightsaber.Release();
+        }
+    }
+}
---
+++ Assets/Scripts/Lightsaber.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightsaber : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _light;
+
+    [SerializeField]
+    private ParticleSystem _particleSystem;
+
+    [SerializeField]
+    private float _openDuration;
+
+    [SerializeField]
+    private float _closeDuration;
+
+    [SerializeField]
+    private float _resetDuration;
+
+    [SerializeField]
+    private AudioSource _audioSource;
+
+    [SerializeField]
+    private AudioSource _audioSourceSecondary;
+
+    [SerializeField]
+    private AudioClip[] _audioClips;
+
+
+    private Vector3 _originalPosition;
+    private Quaternion _originalRotation;
+    private Vector3 _originalScale;
+
+    private bool _free = true;
+    private Vector3 _randomPosition;
+    private float _randomRotation;
+
+    private bool _closed = true;
+    private bool _toggle;
+    private float _targetScale;
+    private float _speed;
+
+
+    private void Awake()
+    {
+        _originalPosition = transform.position;
+        _originalRotation = transform.rotation;
+        _originalScale = transform.localScale;
+    }
+
+    private void Start()
+    {
+        _randomPosition = transform.position;
+        _randomRotation = transform.rotation.eulerAngles.y;
+    }
+
+    private void Update()
+    {
+        if (_free)
+        {
+            if(_randomPosition == transform.position)
+            {
+                _randomPosition = Random.insideUnitSphere * 0.01f;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, _randomPosition, Time.deltaTime * 0.01f);
+
+
+            if (Mathf.Approximately(_randomRotation, transform.rotation.eulerAngles.y))
+            {
+                _randomRotation = Random.Range(45, 145);
+            }
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, _randomRotation, transform.rotation.eulerAngles.z)), Time.deltaTime * 10f);
+        }
+
+        if(_toggle)
+        {
+            float scale = Mathf.MoveTowards(_light.transform.localScale.z, _targetScale, _speed * Time.deltaTime);
+
+            _light.transform.localScale = new Vector3(_light.transform.localScale.x, _light.transform.localScale.y, scale);
+
+            if(scale == _targetScale)
+            {
+                _closed = _targetScale == 0;
+
+                if(!_closed)
+                {
+                    _audioSource.clip = _audioClips[2];
+                    _audioSource.loop = true;
+                    _audioSource.volume = 0.75f;
+                    _audioSource.Play();
+                }
+
+                _toggle = false;
+            }
+        }
+    }
+
+
+    public void Open()
+    {
+        if(_closed)
+        {
+            _audioSource.clip = _audioClips[0];
+            _audioSource.loop = false;
+            _audioSource.volume = 1.0f;
+            _audioSource.Play();
+
+            _targetScale = 1;
+            _speed = Mathf.Abs(_light.transform.localScale.z - _targetScale) / _openDuration;
+            _toggle = true;
+
+            _particleSystem.Play();
+        }
+    }
+
+    public void Swing()
+    {
+        _audioSourceSecondary.Play();
+    }
+
+    public void Close()
+    {
+        if(!_closed)
+        {
+            _audioSource.clip = _audioClips[1];
+            _audioSource.loop = false;
+            _audioSource.volume = 1.0f;
+            _audioSource.Play();
+
+            _targetScale = 0;
+            _speed = Mathf.Abs(_light.transform.localScale.z - _targetScale) / _closeDuration;
+            _toggle = true;
+
+            _particleSystem.Stop();
+        }
+    }
+
+    public void Grab()
+    {
+        _free = false;
+    }
+
+    public void Release()
+    {
+        StartCoroutine(ReleaseCoroutine());
+    }
+
+    private IEnumerator ReleaseCoroutine()
+    {
+        transform.SetParent(null);
+
+        float deltaPosition = Vector3.Distance(transform.position, _originalPosition) / _resetDuration;
+        float deltaRotation = Quaternion.Angle(transform.rotation, _originalRotation) / _resetDuration;
+
+        while (transform.position != _originalPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _originalPosition, deltaPosition * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, _originalRotation, deltaRotation * Time.deltaTime);
+
+            yield return 0;
+        }
+
+        Close();
+
+        _free = true;
+    }
+}
